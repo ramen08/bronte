710af040cc7207c49c76d56327cc774452ea3d53
ramen08 <90885126+ramen08@users.noreply.github.com>
2025-07-11 07:02:24 +0800

    Complete Dog daycare overview section

--- app/public/wp-content/themes/bronte-dogs/templates/home.php
+++ app/public/wp-content/themes/bronte-dogs/templates/home.php
@@ -134,19 +134,31 @@
                             <p><?= wp_kses_post($description); ?></p>
                         <?php endif; ?>
 
-                    <div class="pricing">
-                        <?php if ($full_day_price): ?>
-                            Full Day: <span class="text-green"><?= wp_kses_post($full_day_price); ?></span>
-                        <?php endif; ?>
-                        |
-                        <?php if ($half_day_price): ?>
-                            Half Day: <span class="text-green"><?= wp_kses_post($half_day_price); ?></span>
-                        <?php endif; ?>
-                    </div>
+                        <div class="pricing-container">
+                            <div class="pricing">
+                                <?php if ($full_day_price): ?>
+                                    Full Day: <span class="text-green"><?= wp_kses_post($full_day_price); ?></span>
+                                <?php endif; ?>
+                                |
+                                <?php if ($half_day_price): ?>
+                                    Half Day: <span class="text-green"><?= wp_kses_post($half_day_price); ?></span>
+                                <?php endif; ?>
+                            </div>
 
-                    <?php if ($cta_button_text && $cta_button_link): ?>
-                        <a href="<?= esc_url($cta_button_link); ?>" class="btn"><?= esc_html($cta_button_text); ?></a>
-                    <?php endif; ?>
+                            <?php if ($cta_button_text && $cta_button_link): ?>
+                                <a href="<?= esc_url($cta_button_link); ?>" class="btn"><?= esc_html($cta_button_text); ?></a>
+                            <?php endif; ?>
+
+                            <div class="arrow-and-box">
+                                <div class="arrow">
+                                    <svg width="16" height="75" viewBox="0 0 16 75" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M7.29288 74.7071C7.6834 75.0976 8.31657 75.0976 8.70709 74.7071L15.0711 68.3431C15.4616 67.9526 15.4616 67.3195 15.0711 66.9289C14.6805 66.5384 14.0474 66.5384 13.6568 66.9289L7.99999 72.5858L2.34313 66.9289C1.95261 66.5384 1.31944 66.5384 0.928919 66.9289C0.538395 67.3195 0.538394 67.9526 0.928919 68.3431L7.29288 74.7071ZM8 0L7 -9.68291e-09L6.99999 74L7.99999 74L8.99999 74L9 9.68291e-09L8 0Z" fill="#A4A4A4"/>
+                                    </svg>
+
+                                </div>
+                                <div class="info-box"></div>
+                            </div>
+                        </div>
                     </div>
 
                     <div class="col features-col">
